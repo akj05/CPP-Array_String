e979486f818582cb333c57e5804697c3b14eabb8
Ankush Kumar Jha <jha.akj05@gmail.com>
2025-07-31 10:31:02 +0530

    Concatenation of String.cpp

---
+++ Concatenation of String.cpp
@@ -0,0 +1,26 @@
+// Ankush Jha
+// 24070123016
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str1;
+    string str2;
+    string str3;
+    
+    cout<<"Enter first string\n";
+    cin>>str1;
+    cout<<"Enter second string\n";
+    cin>>str2;
+    str3=str1+" "+str2;
+    cout<<"Concatenated string is "<<str3;
+    return 0;
+}
+
+// OUTPUT
+// Enter first string
+// Ankush
+// Enter second string
+// Jha
+// Concatenated string is Ankush Jha
